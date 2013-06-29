ac2ff174b53774a3f852576c5bd88e78efddd4f8
Ankush Gupta <ankush.me@hotmail.com>
2013-06-29 04:24:51 -0700

    commiting some files

---
+++ src/problem_description.cpp
@@ -0,0 +1,12 @@
+#include <Eigen/Core>
+
+// John's sequential convex optimizer stuff
+#include <sco/expr_ops.hpp>
+#include <sco/modeling_utils.hpp>
+#include <trajopt/trajectory_costs.hpp>
+
+
+using namespace Eigen;
+
+
+
